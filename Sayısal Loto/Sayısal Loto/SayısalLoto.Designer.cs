e858cbe552ccb26f4bbcb592e70007e6cba21585
Metin Pişkin <metin_piskin@outlook.com>
2021-08-07 11:52:50 +0300

    Sayısal Loto
    C# ile yapılmış Sayısal Loto uygulaması

---
+++ Sayısal Loto/Sayısal Loto/SayısalLoto.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Sayısal_Loto
+{
+    partial class SayısalLoto
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SayısalLoto));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.BtnTemizle = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox4 = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(286, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Şonuçla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sayı 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(96, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Sayı 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(177, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 22);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Sayı 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(255, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 22);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Sayı 4";
+            // 
+            // BtnTemizle
+            // 
+            this.BtnTemizle.Enabled = false;
+            this.BtnTemizle.Location = new System.Drawing.Point(16, 220);
+            this.BtnTemizle.Name = "BtnTemizle";
+            this.BtnTemizle.Size = new System.Drawing.Size(286, 52);
+            this.BtnTemizle.TabIndex = 9;
+            this.BtnTemizle.Text = "Temizle";
+            this.BtnTemizle.UseVisualStyleBackColor = true;
+            this.BtnTemizle.Click += new System.EventHandler(this.BtnTemizle_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(96, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(128, 22);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Loto Sonuçları";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(87, 80);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(149, 22);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Sayılarınızı Girin";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(16, 115);
+            this.maskedTextBox1.Mask = "0";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(43, 29);
+            this.maskedTextBox1.TabIndex = 12;
+            this.maskedTextBox1.ValidatingType = typeof(int);
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(100, 115);
+            this.maskedTextBox2.Mask = "0";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(43, 29);
+            this.maskedTextBox2.TabIndex = 13;
+            this.maskedTextBox2.ValidatingType = typeof(int);
+            // 
+            // maskedTextBox3
+            // 
+            this.maskedTextBox3.Location = new System.Drawing.Point(181, 115);
+            this.maskedTextBox3.Mask = "0";
+            this.maskedTextBox3.Name = "maskedTextBox3";
+            this.maskedTextBox3.Size = new System.Drawing.Size(43, 29);
+            this.maskedTextBox3.TabIndex = 14;
+            this.maskedTextBox3.ValidatingType = typeof(int);
+            // 
+            // maskedTextBox4
+            // 
+            this.maskedTextBox4.Location = new System.Drawing.Point(259, 115);
+            this.maskedTextBox4.Mask = "0";
+            this.maskedTextBox4.Name = "maskedTextBox4";
+            this.maskedTextBox4.Size = new System.Drawing.Size(43, 29);
+            this.maskedTextBox4.TabIndex = 15;
+            this.maskedTextBox4.ValidatingType = typeof(int);
+            // 
+            // SayısalLoto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(320, 280);
+            this.Controls.Add(this.maskedTextBox4);
+            this.Controls.Add(this.maskedTextBox3);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.BtnTemizle);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Times New Roman", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.MaximizeBox = false;
+            this.Name = "SayısalLoto";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sayısal Loto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button BtnTemizle;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox3;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox4;
+    }
+}
+
---
+++ Sayısal Loto/Sayısal Loto/SayısalLoto.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sayısal_Loto
+{
+    public partial class SayısalLoto : Form
+    {
+        public SayısalLoto()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rast = new Random();
+            int s1, s2, s3, s4;
+            s1 = rast.Next(1, 10);
+            s2 = rast.Next(1, 10);
+            s3 = rast.Next(1, 10);
+            s4 = rast.Next(1, 10);
+            label1.Text = s1.ToString();
+            label2.Text = s2.ToString();
+            label3.Text = s3.ToString();
+            label4.Text = s4.ToString();
+            //sayı 1
+            if (maskedTextBox1.Text == label1.Text)
+            {
+                maskedTextBox1.BackColor = Color.Green;
+            }
+            else
+            {
+                maskedTextBox1.BackColor = Color.Red;
+            }
+            //sayı 2
+            if (maskedTextBox2.Text == label2.Text)
+            {
+                maskedTextBox2.BackColor = Color.Green;
+            }
+            else
+            {
+                maskedTextBox2.BackColor = Color.Red;
+            }
+            //sayı 3
+            if (maskedTextBox3.Text == label3.Text)
+            {
+                maskedTextBox3.BackColor = Color.Green;
+            }
+            else
+            {
+                maskedTextBox3.BackColor = Color.Red;
+            }
+            //sayı 4
+            if (maskedTextBox4.Text == label4.Text)
+            {
+                maskedTextBox4.BackColor = Color.Green;
+            }
+            else
+            {
+                maskedTextBox4.BackColor = Color.Red;
+            }
+            button1.Enabled = false;
+            BtnTemizle.Enabled = true;
+        }
+
+        private void BtnTemizle_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Sayı 1";
+            label2.Text = "Sayı 2";
+            label3.Text = "Sayı 3";
+            label4.Text = "Sayı 4";
+            maskedTextBox1.Clear();
+            maskedTextBox2.Clear();
+            maskedTextBox3.Clear();
+            maskedTextBox4.Clear();
+            BtnTemizle.Enabled = false;
+            button1.Enabled = true;
+            maskedTextBox1.BackColor = Color.White;
+            maskedTextBox2.BackColor = Color.White;
+            maskedTextBox3.BackColor = Color.White;
+            maskedTextBox4.BackColor = Color.White;
+
+        }
+    }
+}
